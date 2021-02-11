24129541866ba09d495a3623e4a4ebc63d93be0c
Alexandre Yembo <alexandreyembo@Alexandres-MacBook-Pro.local>
2021-02-11 00:36:19 +0000

    Update AssertExtensions.cs

--- Hydra.Core.Tests.Extensions/AssertExtensions.cs
+++ Hydra.Core.Tests.Extensions/AssertExtensions.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
@@ -7,16 +8,26 @@ namespace Hydra.Core.Tests.Extensions
     {
         /// <summary>
         /// Method that create assert for invalid request
-        /// E.g.<!-- Error Message should return as "Invalid request"-->
+        /// E.g. Error Message should return as "Invalid request"
         /// Error code should always be equal 400
         /// </summary>
         /// <param name="actionResult"></param>
         /// <typeparam name="T"></typeparam>
         public static void IsInvalidRequest<T>(this T actionResult) where T: IRequestResult
         {
-            Assert.Equal(400, actionResult.ErrorCode);
+            ReturnFourHundredCode(actionResult);
             Assert.True(actionResult.ErrorMessages.Count > 0);
             Assert.True(actionResult.ErrorMessages.Any(error => error == "Invalid request"));
         }
+
+        public static void IsInvalidRequest<T>(this T actionResult, List<string> expectedErrors) where T : IRequestResult
+        {
+            ReturnFourHundredCode(actionResult);
+            Assert.True(actionResult.ErrorMessages.Count > 0);
+            Assert.Equal(actionResult.ErrorMessages, expectedErrors);
+        }
+
+        private static void ReturnFourHundredCode<T>(T actionResult) where T : IRequestResult =>
+            Assert.Equal(400, actionResult.ErrorCode);
     }
 }
